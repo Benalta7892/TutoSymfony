7a63d7f3c1c586023b7ad8ba430f4bf9199d9a69
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-05-17 12:46:27 +0200

    ajouter/supprimer en BD

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
 use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,11 +14,24 @@ use Symfony\Component\Routing\Attribute\Route;
 class RecipeController extends AbstractController
 {
   #[Route('/recettes', name: 'recipe.index')]
-  public function index(Request $request, RecipeRepository $repository, EntityManagerInterface $em): Response
+  public function index(Request $request, RecipeRepository $repository): Response // ou + EntityManagerInterface $em
   {
     $recipes = $repository->findWithDurationLowerThan(20);
-    $recipes[0]->setTitle('Pâtes bolognaise');
-    $em->flush();
+    // Supprimer une recette
+    // $em->remove($recipes[0]);
+    // $em->flush();
+
+    // Ajouter une nouvelle recette
+    // $recipes = $repository->findWithDurationLowerThan(20);
+    // $recipe = new Recipe();
+    // $recipe->setTitle('Barbe à papa')
+    //   ->setSlug('barbe-papa')
+    //   ->setContent('Pour préparer une délicieuse barbe à papa, commencez par faire fondre du sucre dans une machine à barbe à papa. Ensuite, utilisez un bâton pour enrouler les filaments de sucre autour jusqu\'à obtenir une belle forme. C\'est une friandise parfaite pour les fêtes foraines ou les anniversaires.')
+    //   ->setDuration(2)
+    //   ->setCreatedAt(new \DateTimeImmutable())
+    //   ->setCreatedAt(new \DateTimeImmutable());
+    // $em->persist($recipe);
+    // $em->flush();
     return $this->render('recipe/index.html.twig', ['recipes' => $recipes]);
   }
 
