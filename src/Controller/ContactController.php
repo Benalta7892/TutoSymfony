c581f9cb0086131023c56555b9350bf006525124
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-06-04 14:10:33 +0200

    Gestion des erreurs dans le formulaire de contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -24,23 +24,26 @@ class ContactController extends AbstractController
     $form = $this->createForm(ContactType::class, $data);
     $form->handleRequest($request);
     if ($form->isSubmitted() && $form->isValid()) {
-      $email = (new TemplatedEmail())
-        ->to($data->service)
-        ->from($data->email)
-        ->subject('Demande de contact')
-        ->htmlTemplate('emails/contact.html.twig')
-        ->context(['data' => $data]);
-
-      $mailer->send($email);
-      $this->addFlash(
-        'success',
-        'Votre email a bien été envoyé.'
-      );
-      return $this->redirectToRoute('contact');
+      try {
+        $email = (new TemplatedEmail())
+          ->to($data->service)
+          ->from($data->email)
+          ->subject('Demande de contact')
+          ->htmlTemplate('emails/contact.html.twig')
+          ->context(['data' => $data]);
+        $mailer->send($email);
+        $this->addFlash(
+          'success',
+          'Votre email a bien été envoyé.'
+        );
+        return $this->redirectToRoute('contact');
+      } catch (\Exception $e) {
+        $this->addFlash(
+          'danger',
+          'Impossible d\'envoyer votre email.'
+        );
+      }
     }
-
-
-
     return $this->render('contact/contact.html.twig', [
       'form' => $form,
     ]);
