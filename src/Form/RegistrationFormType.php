d5926379f142048353da5bba769b269a02d839c4
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-07-25 16:47:03 +0200

    Ajout du bouton de soumission et modifications mineur dans le formulaire d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -43,6 +44,8 @@ class RegistrationFormType extends AbstractType
             'max' => 4096,
           ]),
         ],
+      ])->add('save', SubmitType::class, [
+        'label' => 'S\'enregistrer'
       ]);
   }
 
