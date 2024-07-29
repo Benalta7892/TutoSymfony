27bb08b42ea1d321f7413b37477b82fe2446328a
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-07-29 16:52:55 +0200

    Serializer et ajustements du controleur API

--- src/Controller/API/RecipesController.php
+++ src/Controller/API/RecipesController.php
@@ -8,13 +8,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Component\Serializer\SerializerInterface;
 
 
 class RecipesController extends AbstractController
 {
 
   #[Route("/api/recipes", methods: ['GET'])]
-  public function index(RecipeRepository $repository, Request $request)
+  public function index(RecipeRepository $repository, Request $request, SerializerInterface $serializer)
   {
     $recipes = $repository->paginateRecipes($request->query->getInt('page', 1));
     return $this->json($recipes, 200, [], [
