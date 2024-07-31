668c07bba15309ce4830e5401e9444c02f4360ce
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-07-31 16:01:45 +0200

    Configuration de recipeVoter

--- src/Security/Voter/RecipeVoter.php
+++ src/Security/Voter/RecipeVoter.php
@@ -8,39 +8,40 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class RecipeVoter extends Voter
 {
-    public const EDIT = 'POST_EDIT';
-    public const VIEW = 'POST_VIEW';
+  public const EDIT = 'RECIPE_EDIT';
+  public const VIEW = 'RECIPE_VIEW';
 
-    protected function supports(string $attribute, mixed $subject): bool
-    {
-        // replace with your own logic
-        // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, [self::EDIT, self::VIEW])
-            && $subject instanceof \App\Entity\Recipe;
+  protected function supports(string $attribute, mixed $subject): bool
+  {
+    return in_array($attribute, [self::EDIT, self::VIEW])
+      && $subject instanceof \App\Entity\Recipe;
+  }
+
+  protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+  {
+    $user = $token->getUser();
+
+    // if the user is anonymous, do not grant access
+    if (!$user instanceof User) {
+      return false;
     }
 
-    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
-    {
-        $user = $token->getUser();
-
-        // if the user is anonymous, do not grant access
-        if (!$user instanceof UserInterface) {
-            return false;
-        }
-
-        // ... (check conditions and return true to grant permission) ...
-        switch ($attribute) {
-            case self::EDIT:
-                // logic to determine if the user can EDIT
-                // return true or false
-                break;
-
-            case self::VIEW:
-                // logic to determine if the user can VIEW
-                // return true or false
-                break;
-        }
-
-        return false;
+    if (!$subject instanceof Recipe) {
+      return false;
     }
+
+    // ... (check conditions and return true to grant permission) ...
+    switch ($attribute) {
+      case self::EDIT:
+        return $subject->getUser()->getId() === $user->getId();
+        break;
+
+      case self::VIEW:
+        // logic to determine if the user can VIEW
+        // return true or false
+        break;
+    }
+
+    return false;
+  }
 }
