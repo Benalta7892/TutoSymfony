35471fb91d64c06f4982fb388ae4c5375f1c059c
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-01 14:50:21 +0200

    Remplace le champ catégorie par un champ autocomplétion

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -41,11 +41,7 @@ class RecipeType extends AbstractType
         'required' => false,
       ])
       ->add('thumbnailFile', FileType::class)
-      ->add('category', EntityType::class, [
-        'class' => Category::class,
-        'choice_label' => 'name',
-        'autocomplete' => true
-      ])
+      ->add('category', CategoryAutocompleteField::class)
       ->add('content', TextareaType::class, [
         'empty_data' => ''
       ])
