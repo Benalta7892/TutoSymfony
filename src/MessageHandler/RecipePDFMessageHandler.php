2875e4edbbb57beb29dbe58049721ea27a4595f5
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-06 17:53:42 +0200

    Correction de la generation PDF

--- src/MessageHandler/RecipePDFMessageHandler.php
+++ src/MessageHandler/RecipePDFMessageHandler.php
@@ -5,10 +5,9 @@ namespace App\MessageHandler;
 use App\Message\RecipePDFMessage;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-// use Symfony\Component\Process\Exception\ProcessFailedException;
-// use Symfony\Component\Routing\Generator\UrlGenerator;
-// use Symfony\Component\Process\Process;
-// use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[AsMessageHandler]
 final class RecipePDFMessageHandler
@@ -16,9 +15,8 @@ final class RecipePDFMessageHandler
 
   public function __construct(
     #[Autowire('%kernel.project_dir%/public/pdfs')]
-    private readonly string $path
-    // ,
-    // private readonly UrlGeneratorInterface $urlGenerator
+    private readonly string $path,
+    private readonly UrlGeneratorInterface $urlGenerator
   ) {
   }
 
@@ -26,19 +24,19 @@ final class RecipePDFMessageHandler
   public function __invoke(RecipePDFMessage $message): void
   {
     file_put_contents($this->path . '/' . $message->id . '.pdf', '');
-    // $process = new Process([
-    //   'curl',
-    //   '--request',
-    //   'POST',
-    //   'http://localhost:3000/forms/chromium/convert/url',
-    //   '--form',
-    //   'url=' . $this->urlGenerator->generate('recipe.show', ['id' => $message->id], UrlGeneratorInterface::ABSOLUTE_URL),
-    //   '-o',
-    //   $this->path . '/' . $message->id . '.pdf'
-    // ]);
-    // $process->run();
-    // if (!$process->isSuccessful()) {
-    //   throw new ProcessFailedException($process);
-    // }
+    $process = new Process([
+      'curl',
+      '--request',
+      'POST',
+      'http://localhost:3000/forms/chromium/convert/url',
+      '--form',
+      'url=' . $this->urlGenerator->generate('recipes.show', ['id' => $message->id], UrlGeneratorInterface::ABSOLUTE_URL),
+      '-o',
+      $this->path . '/' . $message->id . '.pdf'
+    ]);
+    $process->run();
+    if (!$process->isSuccessful()) {
+      throw new ProcessFailedException($process);
+    }
   }
 }
